be4e86ffa8d3d2877fc206e7c20c4dd61a27f332
Max Lv <max.c.lv@gmail.com>
2013-04-24 09:10:05 +0800

    update binaries

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,3 +1,4 @@
-APP_ABI      := armeabi x86
-APP_PLATFORM := android-9
-APP_STL      := stlport_static
+APP_ABI      			:= armeabi x86
+APP_PLATFORM 			:= android-9
+APP_STL      			:= stlport_static
+NDK_TOOLCHAIN_VERSION 	:= 4.7
