e201615c913730f91a88c15a6a11ffb69d82679d
Max Lv <max.c.lv@gmail.com>
2013-05-20 15:56:52 +0800

    fix the path

--- project/Build.scala
+++ project/Build.scala
@@ -51,7 +51,7 @@ object AndroidBuild extends Build {
   val deploy = TaskKey[Unit]("deploy", "deploy to the build server")
 
   val deployTask = deploy := {
-    "travis-artifacts upload --path target/shadowsocks-" + App.version + ".apk --target-path shadowsocks-" + App.version + ".apk" !;
+    "./deploy target shadowsocks-" + App.version + ".apk" !;
     println("Deploy successfully")
   }
 
