600e6f54286062f57ba4db9e5bd1fa841e82028c
Max Lv <max.c.lv@gmail.com>
2013-06-08 15:49:01 +0800

    remove unnecessary modules

--- build-ndk.sh
+++ build-ndk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd src/main
 ndk-build
-for app in ip6tables iptables obfsproxy pdnsd polipo redsocks shadowsocks tun2socks
+for app in iptables pdnsd redsocks shadowsocks tun2socks
 do
     mv libs/armeabi/$app assets/arm/
     mv libs/x86/$app assets/x86/
