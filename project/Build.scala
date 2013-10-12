78108e12680e797a875da7c14b88b3c4bf0410b0
Max Lv <max.c.lv@gmail.com>
2013-10-12 11:13:49 +0800

    bump version

--- project/Build.scala
+++ project/Build.scala
@@ -5,8 +5,8 @@ import sbtandroid._
 import sbtandroid.AndroidPlugin._
 
 object App {
-  val version = "1.9.0"
-  val versionCode = 50
+  val version = "2.0.0"
+  val versionCode = 51
 }
 
 object General {
