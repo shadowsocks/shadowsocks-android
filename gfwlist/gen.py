c2c7cabbaf7c64349cef94c75feb272a931d6ea1
Max Lv <max.c.lv@gmail.com>
2015-07-30 13:35:04 +0800

    refine dns resolve

--- gfwlist/gen.py
+++ gfwlist/gen.py
@@ -10,20 +10,21 @@ import IPy
 
 def main():
     china_list_set = IPy.IPSet()
-    for line in sys.stdin:
-        line_params = line.split("|")
-        if len(line_params) < 5 or line_params[2] != "ipv4" or line_params[1] != "CN":
-            continue
-        ip_addr = line_params[3]
-        ip_length = float(line_params[4])
-        ip_mask = 32 - int(math.ceil(math.log(ip_length, 2)))
-        china_list_set.add(IPy.IP("%s/%d" % (ip_addr, ip_mask)))
+    # for line in sys.stdin:
+    #     line_params = line.split("|")
+    #     if len(line_params) < 5 or line_params[2] != "ipv4" or line_params[1] != "CN":
+    #         continue
+    #     ip_addr = line_params[3]
+    #     ip_length = float(line_params[4])
+    #     ip_mask = 32 - int(math.ceil(math.log(ip_length, 2)))
+    #     china_list_set.add(IPy.IP("%s/%d" % (ip_addr, ip_mask)))
 
     # 添加内网地址
     internal_list = IPy.IPSet(map(IPy.IP, [
         "0.0.0.0/8",
         "10.0.0.0/8",
         "100.64.0.0/10",
+        "114.114.114.0/24",
         "127.0.0.0/8",
         "169.254.0.0/16",
         "172.16.0.0/12",
@@ -34,6 +35,7 @@ def main():
         "198.18.0.0/15",
         "198.51.100.0/24",
         "203.0.113.0/24",
+        "223.5.5.0/24",
         "224.0.0.0/4",
         "240.0.0.0/4",
     ]))
@@ -45,10 +47,10 @@ def main():
     for ip in china_list_set:
         all.discard(ip)
 
-    filter = itertools.ifilter(lambda x: len(x) <= 65536, all)
-    for ip in filter:
-        all.discard(ip)
-        all.add(IPy.IP(ip.strNormal(0)).make_net('255.255.0.0'))
+    # filter = itertools.ifilter(lambda x: len(x) <= 65536, all)
+    # for ip in filter:
+    #     all.discard(ip)
+    #     all.add(IPy.IP(ip.strNormal(0)).make_net('255.255.0.0'))
 
     # 输出结果
     for ip in all:
