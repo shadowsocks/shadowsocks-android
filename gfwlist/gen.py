f344eb783c72c7126afcca4f123e0aa588d76970
Max Lv <max.c.lv@gmail.com>
2015-08-20 13:06:52 +0800

    update chnroute

--- gfwlist/gen.py
+++ gfwlist/gen.py
@@ -10,14 +10,14 @@ import IPy
 
 def main():
     china_list_set = IPy.IPSet()
-    # for line in sys.stdin:
-    #     line_params = line.split("|")
-    #     if len(line_params) < 5 or line_params[2] != "ipv4" or line_params[1] != "CN":
-    #         continue
-    #     ip_addr = line_params[3]
-    #     ip_length = float(line_params[4])
-    #     ip_mask = 32 - int(math.ceil(math.log(ip_length, 2)))
-    #     china_list_set.add(IPy.IP("%s/%d" % (ip_addr, ip_mask)))
+    for line in sys.stdin:
+        line_params = line.split("|")
+        if len(line_params) < 5 or line_params[2] != "ipv4" or line_params[1] != "CN":
+            continue
+        ip_addr = line_params[3]
+        ip_length = float(line_params[4])
+        ip_mask = 32 - int(math.ceil(math.log(ip_length, 2)))
+        china_list_set.add(IPy.IP("%s/%d" % (ip_addr, ip_mask)))
 
     # 添加内网地址
     internal_list = IPy.IPSet(map(IPy.IP, [
@@ -41,11 +41,13 @@ def main():
     ]))
     china_list_set += internal_list
 
+    all = china_list_set
+
     # 取反
-    all = IPy.IPSet([IPy.IP("0.0.0.0/0")])
+    # all = IPy.IPSet([IPy.IP("0.0.0.0/0")])
     # 剔除所有孤立的C段
-    for ip in china_list_set:
-        all.discard(ip)
+    # for ip in china_list_set:
+    #     all.discard(ip)
 
     # filter = itertools.ifilter(lambda x: len(x) <= 65536, all)
     # for ip in filter:
