1c7ca95b1a82c6e4ac3aa34793609913fb9a4034
Max Lv <max.c.lv@gmail.com>
2015-11-10 09:53:49 +0800

    update setup scripts

--- travis-ci/setup.sh
+++ travis-ci/setup.sh
@@ -14,12 +14,16 @@ if [ ! -d "$ANDROID_HOME" ]
     pushd $HOME/.android
     wget -q http://dl.google.com/android/android-sdk_r24.3.4-linux.tgz
     tar xf android-sdk_r24.3.4-linux.tgz
+    popd
 fi
 
 if [ ! -d "$ANDROID_NDK_HOME" ]
+    mkdir -p $ANDROID_NDK_HOME
+    pushd $HOME/.android
     wget -q http://dl.google.com/android/ndk/android-ndk-r10d-linux-${ARCH}.bin
     chmod +x android-ndk-r10d-linux-${ARCH}.bin
     ./android-ndk-r10d-linux-${ARCH}.bin -y &> /dev/null
+    popd
 fi
 
 echo "y" | android update sdk --filter tools,platform-tools,build-tools-23.0.1,android-23,extra-google-m2repository
