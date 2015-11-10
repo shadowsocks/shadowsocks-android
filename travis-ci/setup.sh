99ff552a74bf7c4966f0f648414ce217d05f09ed
Max Lv <max.c.lv@gmail.com>
2015-11-10 09:51:53 +0800

    udpate travis-ci scripts

--- travis-ci/setup.sh
+++ travis-ci/setup.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+export ARCH=`uname -m`
+export ANDROID_NDK_HOME=$HOME/.android/android-ndk-r10d
+export ANDROID_HOME=$HOME/.android/android-sdk-linux
+export PATH=${ANDROID_NDK_HOME}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools:${PATH}
+
 echo "deb http://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-get update
 sudo apt-get install -y ccache sbt
-export ARCH=`uname -m`
-wget -q http://dl.google.com/android/android-sdk_r24.3.4-linux.tgz
-tar xf android-sdk_r24.3.4-linux.tgz
-echo "y" | android update sdk --filter
-tools,platform-tools,build-tools-23.0.1,android-23,extra-google-m2repository
+
+if [ ! -d "$ANDROID_HOME" ]
+    mkdir -p $ANDROID_HOME
+    pushd $HOME/.android
+    wget -q http://dl.google.com/android/android-sdk_r24.3.4-linux.tgz
+    tar xf android-sdk_r24.3.4-linux.tgz
+fi
+
+if [ ! -d "$ANDROID_NDK_HOME" ]
+    wget -q http://dl.google.com/android/ndk/android-ndk-r10d-linux-${ARCH}.bin
+    chmod +x android-ndk-r10d-linux-${ARCH}.bin
+    ./android-ndk-r10d-linux-${ARCH}.bin -y &> /dev/null
+fi
+
+echo "y" | android update sdk --filter tools,platform-tools,build-tools-23.0.1,android-23,extra-google-m2repository
 --no-ui -a
 echo "y" | android update sdk --filter extra-android-m2repository --no-ui -a
-wget -q http://dl.google.com/android/ndk/android-ndk-r10d-linux-${ARCH}.bin
-chmod +x android-ndk-r10d-linux-${ARCH}.bin
-./android-ndk-r10d-linux-${ARCH}.bin -y &> /dev/null
