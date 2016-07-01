2adaa71ebf15a5ee5e93f90521bf20351a308300
Max Lv <max.c.lv@gmail.com>
2016-07-01 16:02:41 +0800

    Update android ndk

--- travis-ci/setup.sh
+++ travis-ci/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=`uname -m`
-export ANDROID_NDK_HOME=$HOME/.android/android-ndk-r10d
+export ANDROID_NDK_HOME=$HOME/.android/android-ndk-r12
 export ANDROID_HOME=$HOME/.android/android-sdk-linux
 export PATH=${ANDROID_NDK_HOME}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools:${PATH}
 
@@ -16,9 +16,8 @@ fi
 if [ ! -d "$ANDROID_NDK_HOME" ]; then
     mkdir -p $ANDROID_NDK_HOME
     pushd $HOME/.android
-    wget -q http://dl.google.com/android/ndk/android-ndk-r10d-linux-${ARCH}.bin
-    chmod +x android-ndk-r10d-linux-${ARCH}.bin
-    ./android-ndk-r10d-linux-${ARCH}.bin -y &> /dev/null
+    wget -q http://dl.google.com/android/repository/android-ndk-r12-linux-${ARCH}.zip
+    unzip -q android-ndk-r12-linux-${ARCH}.zip
     popd
 fi
 
