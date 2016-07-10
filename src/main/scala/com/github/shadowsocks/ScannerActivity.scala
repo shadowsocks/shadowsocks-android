9919ead15e875eff761564f34f8c71e0bbce36cf
Max Lv <max.c.lv@gmail.com>
2016-07-10 16:04:43 +0800

    Disable AutoFocus and Flash

--- src/main/scala/com/github/shadowsocks/ScannerActivity.scala
+++ src/main/scala/com/github/shadowsocks/ScannerActivity.scala
@@ -50,6 +50,9 @@ class ScannerActivity extends AppCompatActivity with ZXingScannerView.ResultHand
     setupToolbar()
 
     scannerView = new ZXingScannerView(this)
+    scannerView.setAutoFocus(false)
+    scannerView.setFlash(false)
+
     val contentFrame = findViewById(R.id.content_frame).asInstanceOf[ViewGroup]
     contentFrame.addView(scannerView)
   }
