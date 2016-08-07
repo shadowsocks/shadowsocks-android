03c857536d179e3af3e99470b9a43175acb3f180
Max Lv <max.c.lv@gmail.com>
2016-08-07 08:16:55 +0800

    Fix the build scripts

--- build.sh
+++ build.sh
@@ -7,13 +7,16 @@ function try () {
 # Build native binaries
 pushd src/main
 try $ANDROID_NDK_HOME/ndk-build -j8
-# copy executables
+
+rm -rf assets/armeabi-v7a
+rm -rf assets/x86
+mkdir -p assets/armeabi-v7a
+mkdir -p assets/x86
+
+#copy executables
 for app in pdnsd redsocks ss-local ss-tunnel tun2socks
 do
-    rm -rf assets/armeabi-v7a
-    rm -rf assets/x86
-    mkdir -p assets/armeabi-v7a
-    mkdir -p assets/x86
+    echo $app
     try mv libs/armeabi-v7a/$app assets/armeabi-v7a/
     try mv libs/x86/$app assets/x86/
 done
