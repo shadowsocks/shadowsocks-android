a21059ef90e070a1bb1cc4043594d5cc8b286d2f
Max Lv <max.c.lv@gmail.com>
2016-09-09 10:30:22 +0800

    Refine header parser

--- src/main/scala/com/github/shadowsocks/GuardedProcess.scala
+++ src/main/scala/com/github/shadowsocks/GuardedProcess.scala
@@ -94,10 +94,17 @@ class GuardedProcess(cmd: Seq[String]) {
           semaphore.release
           process.waitFor
 
-          if (currentTimeMillis - startTime < 1000) {
-            Log.w(TAG, "process exit too fast, stop guard: " + cmd)
-            isDestroyed = true
+          this.synchronized {
+            if (isRestart) {
+              isRestart = false
+            } else {
+              if (currentTimeMillis - startTime < 1000) {
+                Log.w(TAG, "process exit too fast, stop guard: " + cmd)
+                isDestroyed = true
+              }
+            }
           }
+
         }
       } catch {
         case ignored: InterruptedException =>
@@ -127,8 +134,10 @@ class GuardedProcess(cmd: Seq[String]) {
   }
 
   def restart() {
-    isRestart = true
-    process.destroy()
+    this.synchronized {
+      isRestart = true
+      process.destroy()
+    }
   }
 
   @throws(classOf[InterruptedException])
