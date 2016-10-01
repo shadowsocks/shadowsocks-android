d09d7725e7e710ba2e1e799833faa8eaf949cb42
glzjin <glzjin@zhaojin97.cn>
2016-10-01 23:58:22 +0800

    Revert "delete unused code"
    This reverts commit 0a089f17fc40d216f4dba07c7052c04777bcc2be.

--- src/main/scala/com/github/shadowsocks/BaseService.scala
+++ src/main/scala/com/github/shadowsocks/BaseService.scala
@@ -271,5 +271,15 @@ trait BaseService extends Service {
       state = s
     })
   }
-  
+
+  def getBlackList = {
+    val default = getString(R.string.black_list)
+    try {
+      val container = app.containerHolder.getContainer
+      val update = container.getString("black_list")
+      if (update == null || update.isEmpty) default else update
+    } catch {
+      case ex: Exception => default
+    }
+  }
 }
