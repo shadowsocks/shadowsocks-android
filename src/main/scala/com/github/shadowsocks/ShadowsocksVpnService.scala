8616f97ef9e61120189b90a96f71463bb7dadfd8
glzjin <glzjin@zhaojin97.cn>
2016-10-06 15:28:57 +0800

    change protect mode api version

--- src/main/scala/com/github/shadowsocks/ShadowsocksVpnService.scala
+++ src/main/scala/com/github/shadowsocks/ShadowsocksVpnService.scala
@@ -272,7 +272,7 @@ class ShadowsocksVpnService extends VpnService with BaseService {
 
   def startDnsDaemon() {
     val reject = if (profile.ipv6) "224.0.0.0/3" else "224.0.0.0/3, ::/0"
-    val protect = if (Build.VERSION.SDK_INT >= 21) "protect = \"" + protectPath +"\";" else ""
+    val protect = if (Build.VERSION.SDK_INT >= 19) "protect = \"" + protectPath +"\";" else ""
 
     val conf = profile.route match {
       case Route.BYPASS_CHN | Route.BYPASS_LAN_CHN | Route.GFWLIST => {
