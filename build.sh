45b4886b3dab10bea7bb9dd1754502ad12683133
esdeathlove <esdeathlove@gmail.com>
2017-02-26 21:32:58 +0800

    arm64

--- build.sh
+++ build.sh
@@ -11,13 +11,16 @@ try pushd src/main
 try ${ANDROID_NDK_HOME}/ndk-build -j8
 
 # copy executables
+rm -rf assets/arm64-v8a
 rm -rf assets/armeabi-v7a
 rm -rf assets/x86
+mkdir -p assets/arm64-v8a
 mkdir -p assets/armeabi-v7a
 mkdir -p assets/x86
 for app in pdnsd redsocks ss-local ss-tunnel tun2socks
 do
     try mv libs/armeabi-v7a/$app assets/armeabi-v7a/
+    try mv libs/arm64-v8a/$app assets/arm64-v8a/
     try mv libs/x86/$app assets/x86/
 done
 
