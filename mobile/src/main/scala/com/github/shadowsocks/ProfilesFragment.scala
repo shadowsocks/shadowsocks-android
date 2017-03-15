387d6ad6f7f4949d710dbea16749bf7d61f699bb
binglinda <binglinda@gmail.com>
2017-03-15 21:26:06 +0800

    modify

--- mobile/src/main/scala/com/github/shadowsocks/ProfilesFragment.scala
+++ mobile/src/main/scala/com/github/shadowsocks/ProfilesFragment.scala
@@ -278,7 +278,6 @@ final class ProfilesFragment extends ToolbarFragment with Toolbar.OnMenuItemClic
     toolbar.inflateMenu(R.menu.profile_manager_menu)
     toolbar.setOnMenuItemClickListener(this)
 
-    if (app.profileManager.getFirstProfile.isEmpty) app.profileId(app.profileManager.createProfile().id)
     val profilesList = view.findViewById(R.id.list).asInstanceOf[RecyclerView]
     val layoutManager = new LinearLayoutManager(getActivity, LinearLayoutManager.VERTICAL, false)
     profilesList.setLayoutManager(layoutManager)
