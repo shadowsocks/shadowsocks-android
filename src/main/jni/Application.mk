2c2ae8397484ec915d9505ea219d71d5bf9b2fd8
esdeathlove <esdeathlove@gmail.com>
2017-03-24 20:12:32 +0800

    update ssr libev & bump version

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -1,4 +1,5 @@
 APP_ABI      			:= arm64-v8a armeabi-v7a x86
-APP_PLATFORM 			:= android-25
+APP_PLATFORM 			:= android-21
 APP_STL      			:= stlport_static
 NDK_TOOLCHAIN_VERSION 	:= clang
+APP_OPTIM					:= release
