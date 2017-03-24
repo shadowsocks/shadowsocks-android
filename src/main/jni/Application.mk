06c4da54a034d5a8ccbcc9e4ac8fcac9d2fec12c
esdeathlove <esdeathlove@gmail.com>
2017-03-25 00:37:09 +0800

    Revert "update ssr libev & bump version"
    This reverts commit 2c2ae8397484ec915d9505ea219d71d5bf9b2fd8.

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -1,5 +1,4 @@
 APP_ABI      			:= arm64-v8a armeabi-v7a x86
-APP_PLATFORM 			:= android-21
+APP_PLATFORM 			:= android-25
 APP_STL      			:= stlport_static
 NDK_TOOLCHAIN_VERSION 	:= clang
-APP_OPTIM					:= release
