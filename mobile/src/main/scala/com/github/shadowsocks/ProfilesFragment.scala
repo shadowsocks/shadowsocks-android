f5036bbde5bb51e1dfd1207fb0b232ac02213d2d
Mygod <contact-git@mygod.be>
2017-11-19 01:16:53 -0800

    Refrain from using SMART_BANNER to prevent its weirdness

--- mobile/src/main/scala/com/github/shadowsocks/ProfilesFragment.scala
+++ mobile/src/main/scala/com/github/shadowsocks/ProfilesFragment.scala
@@ -21,7 +21,6 @@
 package com.github.shadowsocks
 
 import android.content._
-import android.content.res.Configuration
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.widget.RecyclerView.ViewHolder
@@ -137,13 +136,9 @@ final class ProfilesFragment extends ToolbarFragment with Toolbar.OnMenuItemClic
           adView = new AdView(getActivity)
           adView.setLayoutParams(params)
           adView.setAdUnitId("ca-app-pub-9097031975646651/7760346322")
+          adView.setAdSize(AdSize.FLUID)
 
-          if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
-            adView.setAdSize(AdSize.FULL_BANNER)
-          else
-            adView.setAdSize(AdSize.SMART_BANNER)
-
-          itemView.findViewById[LinearLayout](R.id.indicator).addView(adView)
+          itemView.findViewById[LinearLayout](R.id.content).addView(adView)
 
           // Load Ad
           val adBuilder = new AdRequest.Builder()
