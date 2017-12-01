ae833210c620e80324fb3ae8d50a5ec256ac63ee
Mygod <contact-git@mygod.be>
2017-11-30 21:49:26 -0800

    Revert to minimal libsodium source fileset

--- mobile/src/main/jni/Android.mk
+++ mobile/src/main/jni/Android.mk
@@ -27,89 +27,34 @@ include $(CLEAR_VARS)
 SODIUM_SOURCE := \
 	crypto_aead/chacha20poly1305/sodium/aead_chacha20poly1305.c \
 	crypto_aead/xchacha20poly1305/sodium/aead_xchacha20poly1305.c \
-	crypto_auth/crypto_auth.c \
-	crypto_auth/hmacsha256/auth_hmacsha256.c \
-	crypto_auth/hmacsha512/auth_hmacsha512.c \
-	crypto_auth/hmacsha512256/auth_hmacsha512256.c \
-	crypto_box/crypto_box.c \
-	crypto_box/crypto_box_easy.c \
-	crypto_box/crypto_box_seal.c \
-	crypto_box/curve25519xsalsa20poly1305/box_curve25519xsalsa20poly1305.c \
 	crypto_core/curve25519/ref10/curve25519_ref10.c \
 	crypto_core/hchacha20/core_hchacha20.c \
-	crypto_core/hsalsa20/ref2/core_hsalsa20_ref2.c \
-	crypto_core/hsalsa20/core_hsalsa20.c \
 	crypto_core/salsa/ref/core_salsa_ref.c \
-	crypto_generichash/crypto_generichash.c \
-	crypto_generichash/blake2b/generichash_blake2.c \
 	crypto_generichash/blake2b/ref/blake2b-compress-ref.c \
 	crypto_generichash/blake2b/ref/blake2b-ref.c \
 	crypto_generichash/blake2b/ref/generichash_blake2b.c \
-	crypto_hash/crypto_hash.c \
-	crypto_hash/sha256/hash_sha256.c \
-	crypto_hash/sha256/cp/hash_sha256_cp.c \
-	crypto_hash/sha512/hash_sha512.c \
-	crypto_hash/sha512/cp/hash_sha512_cp.c \
-	crypto_kdf/blake2b/kdf_blake2b.c \
-	crypto_kdf/crypto_kdf.c \
-	crypto_kx/crypto_kx.c \
-	crypto_onetimeauth/crypto_onetimeauth.c \
 	crypto_onetimeauth/poly1305/onetimeauth_poly1305.c \
 	crypto_onetimeauth/poly1305/donna/poly1305_donna.c \
+	crypto_pwhash/crypto_pwhash.c \
 	crypto_pwhash/argon2/argon2-core.c \
+	crypto_pwhash/argon2/argon2.c \
 	crypto_pwhash/argon2/argon2-encoding.c \
 	crypto_pwhash/argon2/argon2-fill-block-ref.c \
-	crypto_pwhash/argon2/argon2.c \
 	crypto_pwhash/argon2/blake2b-long.c \
 	crypto_pwhash/argon2/pwhash_argon2i.c \
-	crypto_pwhash/argon2/pwhash_argon2id.c \
-	crypto_pwhash/crypto_pwhash.c \
-	crypto_scalarmult/crypto_scalarmult.c \
 	crypto_scalarmult/curve25519/scalarmult_curve25519.c \
-	crypto_secretbox/crypto_secretbox.c \
-	crypto_secretbox/crypto_secretbox_easy.c \
-	crypto_secretbox/xsalsa20poly1305/secretbox_xsalsa20poly1305.c \
-	crypto_secretstream/xchacha20poly1305/secretstream_xchacha20poly1305.c \
-	crypto_shorthash/crypto_shorthash.c \
-	crypto_shorthash/siphash24/shorthash_siphash24.c \
-	crypto_shorthash/siphash24/ref/shorthash_siphash24_ref.c \
-	crypto_sign/crypto_sign.c \
-	crypto_sign/ed25519/sign_ed25519.c \
-	crypto_sign/ed25519/ref10/keypair.c \
-	crypto_sign/ed25519/ref10/open.c \
-	crypto_sign/ed25519/ref10/sign.c \
+	crypto_scalarmult/curve25519/ref10/x25519_ref10.c \
 	crypto_stream/chacha20/stream_chacha20.c \
 	crypto_stream/chacha20/ref/chacha20_ref.c \
-	crypto_stream/crypto_stream.c \
 	crypto_stream/salsa20/stream_salsa20.c \
-	crypto_stream/xsalsa20/stream_xsalsa20.c \
+	crypto_stream/salsa20/ref/salsa20_ref.c \
 	crypto_verify/sodium/verify.c \
 	randombytes/randombytes.c \
-	sodium/codecs.c \
+	randombytes/sysrandom/randombytes_sysrandom.c \
 	sodium/core.c \
 	sodium/runtime.c \
 	sodium/utils.c \
-	sodium/version.c \
-	randombytes/salsa20/randombytes_salsa20_random.c \
-	randombytes/sysrandom/randombytes_sysrandom.c \
-	crypto_scalarmult/curve25519/ref10/x25519_ref10.c \
-	crypto_stream/salsa20/ref/salsa20_ref.c \
-	crypto_box/curve25519xchacha20poly1305/box_curve25519xchacha20poly1305.c \
-	crypto_box/curve25519xchacha20poly1305/box_seal_curve25519xchacha20poly1305.c \
-	crypto_pwhash/scryptsalsa208sha256/crypto_scrypt-common.c \
-	crypto_pwhash/scryptsalsa208sha256/scrypt_platform.c \
-	crypto_pwhash/scryptsalsa208sha256/pbkdf2-sha256.c \
-	crypto_pwhash/scryptsalsa208sha256/pwhash_scryptsalsa208sha256.c \
-	crypto_pwhash/scryptsalsa208sha256/nosse/pwhash_scryptsalsa208sha256_nosse.c \
-	crypto_secretbox/xchacha20poly1305/secretbox_xchacha20poly1305.c \
-	crypto_shorthash/siphash24/shorthash_siphashx24.c \
-	crypto_shorthash/siphash24/ref/shorthash_siphashx24_ref.c \
-	crypto_sign/ed25519/ref10/obsolete.c \
-	crypto_stream/salsa2012/ref/stream_salsa2012_ref.c \
-	crypto_stream/salsa2012/stream_salsa2012.c \
-	crypto_stream/salsa208/ref/stream_salsa208_ref.c \
-	crypto_stream/salsa208/stream_salsa208.c \
-	crypto_stream/xchacha20/stream_xchacha20.c
+	sodium/version.c
 
 LOCAL_MODULE := sodium
 LOCAL_CFLAGS += -I$(LOCAL_PATH)/libsodium/src/libsodium/include \
