5c75148813d992e1a38a5c87a9de7863481b76d0
Mygod <contact-git@mygod.be>
2017-11-30 21:49:41 -0800

    Add support for downgrading the database for whatever reason
    And it simply kills all the data.

--- mobile/src/main/scala/com/github/shadowsocks/database/DBHelper.scala
+++ mobile/src/main/scala/com/github/shadowsocks/database/DBHelper.scala
@@ -27,8 +27,8 @@ import android.content.pm.ApplicationInfo
 import android.database.sqlite.SQLiteDatabase
 import android.preference.PreferenceManager
 import com.github.shadowsocks.ShadowsocksApplication.app
-import com.github.shadowsocks.preference.OrmLitePreferenceDataStore
 import com.github.shadowsocks.utils.Key
+import com.j256.ormlite.android.AndroidDatabaseConnection
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper
 import com.j256.ormlite.dao.Dao
 import com.j256.ormlite.support.ConnectionSource
@@ -68,96 +68,99 @@ class DBHelper(val context: Context)
     onCreate(database, connectionSource)
   }
 
-  def onUpgrade(database: SQLiteDatabase, connectionSource: ConnectionSource, oldVersion: Int,
-    newVersion: Int) {
-    if (oldVersion != newVersion) {
-      if (oldVersion < 7) {
-        recreate(database, connectionSource)
-        return
-      }
-
-      try {
-        if (oldVersion < 8) {
-          profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN udpdns SMALLINT;")
-        }
-        if (oldVersion < 9) {
-          profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN route VARCHAR DEFAULT 'all';")
-        } else if (oldVersion < 19) {
-          profileDao.executeRawNoArgs("UPDATE `profile` SET route = 'all' WHERE route IS NULL;")
-        }
-        if (oldVersion < 11) {
-          profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN ipv6 SMALLINT;")
-        }
-        if (oldVersion < 12) {
-          profileDao.executeRawNoArgs("BEGIN TRANSACTION;")
-          profileDao.executeRawNoArgs("ALTER TABLE `profile` RENAME TO `tmp`;")
-          TableUtils.createTable(connectionSource, classOf[Profile])
-          profileDao.executeRawNoArgs(
-            "INSERT INTO `profile`(id, name, host, localPort, remotePort, password, method, route, proxyApps, bypass," +
-              " udpdns, ipv6, individual) " +
-            "SELECT id, name, host, localPort, remotePort, password, method, route, 1 - global, bypass, udpdns, ipv6," +
-            " individual FROM `tmp`;")
-          profileDao.executeRawNoArgs("DROP TABLE `tmp`;")
-          profileDao.executeRawNoArgs("COMMIT;")
-        } else if (oldVersion < 13) {
-          profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN tx LONG;")
-          profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN rx LONG;")
-          profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN date VARCHAR;")
-        }
-
-        if (oldVersion < 15) {
-          if (oldVersion >= 12) profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN userOrder LONG;")
-          var i = 0
-          for (profile <- profileDao.queryForAll.asScala) {
-            if (oldVersion < 14) profile.individual = updateProxiedApps(context, profile.individual)
-            profile.userOrder = i
-            profileDao.update(profile)
-            i += 1
-          }
-        }
-
-        if (oldVersion < 16) {
-          profileDao.executeRawNoArgs("UPDATE `profile` SET route = 'bypass-lan-china' WHERE route = 'bypass-china'")
-        }
-
-        if (oldVersion < 21) {
-          profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN remoteDns VARCHAR DEFAULT '8.8.8.8';")
-        }
-
-        if (oldVersion < 17) {
-          profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN plugin VARCHAR;")
-        } else if (oldVersion < 22) {
-          // upgrade kcptun to SIP003 plugin
-          profileDao.executeRawNoArgs("BEGIN TRANSACTION;")
-          profileDao.executeRawNoArgs("ALTER TABLE `profile` RENAME TO `tmp`;")
-          TableUtils.createTable(connectionSource, classOf[Profile])
-          profileDao.executeRawNoArgs(
-            "INSERT INTO `profile`(id, name, host, localPort, remotePort, password, method, route, remoteDns, " +
-            "proxyApps, bypass, udpdns, ipv6, individual, tx, rx, date, userOrder, plugin) " +
-            "SELECT id, name, host, localPort, CASE WHEN kcp = 1 THEN kcpPort ELSE remotePort END, password, method, " +
-            "route, remoteDns, proxyApps, bypass, udpdns, ipv6, individual, tx, rx, date, userOrder, " +
-            "CASE WHEN kcp = 1 THEN 'kcptun ' || kcpcli ELSE NULL END FROM `tmp`;")
-          profileDao.executeRawNoArgs("DROP TABLE `tmp`;")
-          profileDao.executeRawNoArgs("COMMIT;")
-        }
-
-        if (oldVersion < 23) {
-          profileDao.executeRawNoArgs("BEGIN TRANSACTION;")
-          TableUtils.createTable(connectionSource, classOf[KeyValuePair])
-          profileDao.executeRawNoArgs("COMMIT;")
-          import KeyValuePair._
-          val old = PreferenceManager.getDefaultSharedPreferences(app)
-          kvPairDao.createOrUpdate(new KeyValuePair(Key.id, TYPE_INT,
-            ByteBuffer.allocate(4).putInt(old.getInt(Key.id, 0)).array()))
-          kvPairDao.createOrUpdate(new KeyValuePair(Key.tfo, TYPE_BOOLEAN,
-            ByteBuffer.allocate(1).put((if (old.getBoolean(Key.tfo, false)) 1 else 0).toByte).array()))
-        }
-      } catch {
-        case ex: Exception =>
-          app.track(ex)
-          recreate(database, connectionSource)
-          return
-      }
+  def onUpgrade(database: SQLiteDatabase, connectionSource: ConnectionSource, oldVersion: Int, newVersion: Int) {
+    if (oldVersion < 7) {
+      recreate(database, connectionSource)
+      return
     }
+
+    try {
+      if (oldVersion < 8) {
+        profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN udpdns SMALLINT;")
+      }
+      if (oldVersion < 9) {
+        profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN route VARCHAR DEFAULT 'all';")
+      } else if (oldVersion < 19) {
+        profileDao.executeRawNoArgs("UPDATE `profile` SET route = 'all' WHERE route IS NULL;")
+      }
+      if (oldVersion < 11) {
+        profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN ipv6 SMALLINT;")
+      }
+      if (oldVersion < 12) {
+        profileDao.executeRawNoArgs("BEGIN TRANSACTION;")
+        profileDao.executeRawNoArgs("ALTER TABLE `profile` RENAME TO `tmp`;")
+        TableUtils.createTable(connectionSource, classOf[Profile])
+        profileDao.executeRawNoArgs(
+          "INSERT INTO `profile`(id, name, host, localPort, remotePort, password, method, route, proxyApps, bypass," +
+            " udpdns, ipv6, individual) " +
+          "SELECT id, name, host, localPort, remotePort, password, method, route, 1 - global, bypass, udpdns, ipv6," +
+          " individual FROM `tmp`;")
+        profileDao.executeRawNoArgs("DROP TABLE `tmp`;")
+        profileDao.executeRawNoArgs("COMMIT;")
+      } else if (oldVersion < 13) {
+        profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN tx LONG;")
+        profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN rx LONG;")
+        profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN date VARCHAR;")
+      }
+
+      if (oldVersion < 15) {
+        if (oldVersion >= 12) profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN userOrder LONG;")
+        var i = 0
+        for (profile <- profileDao.queryForAll.asScala) {
+          if (oldVersion < 14) profile.individual = updateProxiedApps(context, profile.individual)
+          profile.userOrder = i
+          profileDao.update(profile)
+          i += 1
+        }
+      }
+
+      if (oldVersion < 16) {
+        profileDao.executeRawNoArgs("UPDATE `profile` SET route = 'bypass-lan-china' WHERE route = 'bypass-china'")
+      }
+
+      if (oldVersion < 21) {
+        profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN remoteDns VARCHAR DEFAULT '8.8.8.8';")
+      }
+
+      if (oldVersion < 17) {
+        profileDao.executeRawNoArgs("ALTER TABLE `profile` ADD COLUMN plugin VARCHAR;")
+      } else if (oldVersion < 22) {
+        // upgrade kcptun to SIP003 plugin
+        profileDao.executeRawNoArgs("BEGIN TRANSACTION;")
+        profileDao.executeRawNoArgs("ALTER TABLE `profile` RENAME TO `tmp`;")
+        TableUtils.createTable(connectionSource, classOf[Profile])
+        profileDao.executeRawNoArgs(
+          "INSERT INTO `profile`(id, name, host, localPort, remotePort, password, method, route, remoteDns, " +
+          "proxyApps, bypass, udpdns, ipv6, individual, tx, rx, date, userOrder, plugin) " +
+          "SELECT id, name, host, localPort, CASE WHEN kcp = 1 THEN kcpPort ELSE remotePort END, password, method, " +
+          "route, remoteDns, proxyApps, bypass, udpdns, ipv6, individual, tx, rx, date, userOrder, " +
+          "CASE WHEN kcp = 1 THEN 'kcptun ' || kcpcli ELSE NULL END FROM `tmp`;")
+        profileDao.executeRawNoArgs("DROP TABLE `tmp`;")
+        profileDao.executeRawNoArgs("COMMIT;")
+      }
+
+      if (oldVersion < 23) {
+        profileDao.executeRawNoArgs("BEGIN TRANSACTION;")
+        TableUtils.createTable(connectionSource, classOf[KeyValuePair])
+        profileDao.executeRawNoArgs("COMMIT;")
+        import KeyValuePair._
+        val old = PreferenceManager.getDefaultSharedPreferences(app)
+        kvPairDao.createOrUpdate(new KeyValuePair(Key.id, TYPE_INT,
+          ByteBuffer.allocate(4).putInt(old.getInt(Key.id, 0)).array()))
+        kvPairDao.createOrUpdate(new KeyValuePair(Key.tfo, TYPE_BOOLEAN,
+          ByteBuffer.allocate(1).put((if (old.getBoolean(Key.tfo, false)) 1 else 0).toByte).array()))
+      }
+    } catch {
+      case ex: Exception =>
+        app.track(ex)
+        recreate(database, connectionSource)
+    }
+  }
+
+  override def onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+    val connection = new AndroidDatabaseConnection(db, true)
+    connectionSource.saveSpecialConnection(connection)
+    recreate(db, connectionSource)
+    connectionSource.clearSpecialConnection(connection)
   }
 }
