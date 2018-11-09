ae4d0e75bf91d3bb0199424914c7515186375b7d
^_^void <xmh19936688@163.com>
2018-11-09 09:24:41 +0800

    make code clear

--- core/src/main/java/com/github/shadowsocks/bg/ServiceNotification.kt
+++ core/src/main/java/com/github/shadowsocks/bg/ServiceNotification.kt
@@ -82,7 +82,7 @@ class ServiceNotification(private val service: BaseService.Interface, profileNam
         service as Context
         if (Build.VERSION.SDK_INT < 24) builder.addAction(R.drawable.ic_navigation_close,
                 service.getString(R.string.stop), PendingIntent.getBroadcast(service, 0, Intent(Action.CLOSE), 0))
-        update(if (service.getSystemService<PowerManager>()?.isInteractive != false)
+        update(if (service.getSystemService<PowerManager>()?.isInteractive)
             Intent.ACTION_SCREEN_ON else Intent.ACTION_SCREEN_OFF, true)
         val screenFilter = IntentFilter()
         screenFilter.addAction(Intent.ACTION_SCREEN_ON)
