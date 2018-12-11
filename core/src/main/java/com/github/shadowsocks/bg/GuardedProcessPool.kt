8e94b2efcd81e19d05d4633e31f5d31c03660dfc
Mygod <contact-github@mygod.be>
2018-12-11 14:11:50 +0800

    Fix class not found

--- core/src/main/java/com/github/shadowsocks/bg/GuardedProcessPool.kt
+++ core/src/main/java/com/github/shadowsocks/bg/GuardedProcessPool.kt
@@ -38,7 +38,7 @@ import java.util.concurrent.atomic.AtomicReference
 class GuardedProcessPool {
     companion object Dummy : IOException("Oopsie the developer has made a no-no") {
         private const val TAG = "GuardedProcessPool"
-        private val ProcessImpl by lazy { Class.forName("java/lang/ProcessManager\$ProcessImpl") }
+        private val ProcessImpl by lazy { Class.forName("java.lang.ProcessManager\$ProcessImpl") }
         private val pid by lazy { ProcessImpl.getField("pid").apply { isAccessible = true } }
         private val exitValueMutex by lazy { ProcessImpl.getField("exitValueMutex").apply { isAccessible = true } }
     }
