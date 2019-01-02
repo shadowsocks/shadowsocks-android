74f7726c128abe9587cadd201d6d0cdf24b361c8
Max Lv <max.c.lv@gmail.com>
2019-01-02 12:05:46 +0800

    Add ACL file for domain exception in overture

--- core/src/main/java/com/github/shadowsocks/bg/LocalDnsService.kt
+++ core/src/main/java/com/github/shadowsocks/bg/LocalDnsService.kt
@@ -70,6 +70,7 @@ object LocalDnsService {
                             put("PrimaryDNS", localDns)
                             put("AlternativeDNS", remoteDns)
                             put("IPNetworkFile", "china_ip_list.txt")
+                            put("DomainFile", "dns.acl")
                         }
                         Acl.CHINALIST -> {
                             put("PrimaryDNS", localDns)
