0bac32b77e8a22f3d5f09d106e8825792d868326
Mygod <contact-github@mygod.be>
2019-01-14 18:02:26 +0800

    Add escape test for PluginOptions

--- plugin/src/test/java/com/github/shadowsocks/plugin/PluginOptionsTest.kt
+++ plugin/src/test/java/com/github/shadowsocks/plugin/PluginOptionsTest.kt
@@ -25,7 +25,7 @@ import org.junit.Test
 
 class PluginOptionsTest {
     @Test
-    fun equal() {
+    fun basic() {
         val o1 = PluginOptions("obfs-local;obfs=http;obfs-host=localhost")
         val o2 = PluginOptions("obfs-local", "obfs-host=localhost;obfs=http")
         Assert.assertEquals(o1.hashCode(), o2.hashCode())
@@ -34,5 +34,20 @@ class PluginOptionsTest {
         Assert.assertEquals(true, o2 == o3)
         val o4 = PluginOptions(o2.id, o2.toString())
         Assert.assertEquals(true, o3 == o4)
+    }
+
+    @Test
+    fun escape() {
+        val options = PluginOptions("escapeTest")
+        options["subject"] = "value;semicolon"
+        Assert.assertEquals(true, options == PluginOptions(options.toString(false)))
+        options["key;semicolon"] = "object"
+        Assert.assertEquals(true, options == PluginOptions(options.toString(false)))
+        options["subject"] = "value=equals"
+        Assert.assertEquals(true, options == PluginOptions(options.toString(false)))
+        options["key=equals"] = "object"
+        Assert.assertEquals(true, options == PluginOptions(options.toString(false)))
+        options["advanced\\=;test"] = "in;=\\progress"
+        Assert.assertEquals(true, options == PluginOptions(options.toString(false)))
     }
 }
