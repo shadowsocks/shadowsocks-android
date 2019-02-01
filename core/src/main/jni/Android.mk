bb6b2d6177a89e73a76d403ef92f30659059113a
Mygod <contact-github@mygod.be>
2019-02-01 14:10:30 +0800

    Intercept DNS packets only at 172.19.0.2:53

--- core/src/main/jni/Android.mk
+++ core/src/main/jni/Android.mk
@@ -325,7 +325,6 @@ TUN2SOCKS_SOURCES := \
         flow/PacketProtoDecoder.c \
         socksclient/BSocksClient.c \
         tuntap/BTap.c \
-        lwip/src/core/timers.c \
         lwip/src/core/udp.c \
         lwip/src/core/memp.c \
         lwip/src/core/init.c \
@@ -334,14 +333,16 @@ TUN2SOCKS_SOURCES := \
         lwip/src/core/tcp_out.c \
         lwip/src/core/netif.c \
         lwip/src/core/def.c \
+        lwip/src/core/ip.c \
         lwip/src/core/mem.c \
         lwip/src/core/tcp_in.c \
         lwip/src/core/stats.c \
         lwip/src/core/inet_chksum.c \
+        lwip/src/core/timeouts.c \
         lwip/src/core/ipv4/icmp.c \
         lwip/src/core/ipv4/igmp.c \
         lwip/src/core/ipv4/ip4_addr.c \
-        lwip/src/core/ipv4/ip_frag.c \
+        lwip/src/core/ipv4/ip4_frag.c \
         lwip/src/core/ipv4/ip4.c \
         lwip/src/core/ipv4/autoip.c \
         lwip/src/core/ipv6/ethip6.c \
