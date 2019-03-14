6b60d91b4be93bff7bee04dede031a622772ea84
Mygod <contact-github@mygod.be>
2019-03-14 17:18:59 +0800

    Add copyright notice

--- translate.py
+++ translate.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 """
+Copyright (C) 2019 by Max Lv <max.c.lv@gmail.com>
+Copyright (C) 2019 by Mygod Studio <contact-shadowsocks-android@mygod.be>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 Usage: ./translate.py <API token>
 Prerequisite: pip install poeditor
 See also: https://poeditor.com/docs/languages
