e0d20ee5f07c906a3ca86f3eeeface8920e7ae03
Mygod <contact-git@mygod.be>
2019-05-19 09:50:40 +0800

    Remove unnecessary synchronization

--- core/src/main/java/com/github/shadowsocks/utils/SingleInstanceActivity.kt
+++ core/src/main/java/com/github/shadowsocks/utils/SingleInstanceActivity.kt
@@ -1,17 +1,17 @@
 package com.github.shadowsocks.utils
 
 import androidx.activity.ComponentActivity
+import androidx.annotation.MainThread
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
-import java.util.*
-import java.util.concurrent.ConcurrentHashMap
 
 /**
  * See also: https://stackoverflow.com/a/30821062/2245107
  */
 object SingleInstanceActivity : DefaultLifecycleObserver {
-    private val active = Collections.newSetFromMap(ConcurrentHashMap<Class<LifecycleOwner>, Boolean>())
+    private val active = mutableSetOf<Class<LifecycleOwner>>()
 
+    @MainThread
     fun register(activity: ComponentActivity) = if (active.add(activity.javaClass)) apply {
         activity.lifecycle.addObserver(this)
     } else {
