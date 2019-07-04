44f37d141f21bc2219dfdf03512e0b915a1a999f
Mygod <contact-git@mygod.be>
2019-07-05 02:19:13 +0800

    Suppress DnsExceptions

--- core/src/main/java/com/github/shadowsocks/net/LocalDnsServer.kt
+++ core/src/main/java/com/github/shadowsocks/net/LocalDnsServer.kt
@@ -20,6 +20,8 @@
 
 package com.github.shadowsocks.net
 
+import android.net.DnsResolver
+import android.os.Build
 import android.util.Log
 import com.crashlytics.android.Crashlytics
 import com.github.shadowsocks.utils.printLog
@@ -142,8 +144,9 @@ class LocalDnsServer(private val localResolver: suspend (String) -> Array<InetAd
                 when (e) {
                     is TimeoutCancellationException -> Crashlytics.log(Log.WARN, TAG, "Remote resolving timed out")
                     is CancellationException -> { } // ignore
-                    is IOException -> Crashlytics.log(Log.WARN, TAG, e.message)
-                    else -> printLog(e)
+                    else -> if (e is IOException || Build.VERSION.SDK_INT >= 29 && e is DnsResolver.DnsException) {
+                        Crashlytics.log(Log.WARN, TAG, e.message)
+                    } else printLog(e)
                 }
                 ByteBuffer.wrap(prepareDnsResponse(request).apply {
                     header.rcode = Rcode.SERVFAIL
