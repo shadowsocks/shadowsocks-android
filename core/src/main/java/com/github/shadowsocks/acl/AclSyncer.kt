f36a0adfcf687a72f7fca220a7c7f4d48f8b5a8d
Mygod <contact-git@mygod.be>
2019-07-14 11:49:55 +0800

    Allow ACL sync to be cancelled

--- core/src/main/java/com/github/shadowsocks/acl/AclSyncer.kt
+++ core/src/main/java/com/github/shadowsocks/acl/AclSyncer.kt
@@ -23,8 +23,9 @@ package com.github.shadowsocks.acl
 import android.content.Context
 import androidx.work.*
 import com.github.shadowsocks.Core
-import kotlinx.coroutines.Dispatchers
+import com.github.shadowsocks.utils.useCancellable
 import java.io.IOException
+import java.net.HttpURLConnection
 import java.net.URL
 import java.util.concurrent.TimeUnit
 
@@ -44,12 +45,10 @@ class AclSyncer(context: Context, workerParams: WorkerParameters) : CoroutineWor
         })
     }
 
-    override val coroutineContext get() = Dispatchers.IO
-
     override suspend fun doWork(): Result = try {
         val route = inputData.getString(KEY_ROUTE)!!
-        val acl = URL("https://shadowsocks.org/acl/android/v1/$route.acl").openStream().bufferedReader()
-                .use { it.readText() }
+        val connection = URL("https://shadowsocks.org/acl/android/v1/$route.acl").openConnection() as HttpURLConnection
+        val acl = connection.useCancellable { inputStream.bufferedReader().use { it.readText() } }
         Acl.getFile(route).printWriter().use { it.write(acl) }
         Result.success()
     } catch (e: IOException) {
