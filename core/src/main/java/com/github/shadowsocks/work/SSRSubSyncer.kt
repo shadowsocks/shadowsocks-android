e8a26c80390ae62cd4e2d817bc44f1a38297bb02
shadowsocksRb <56433352+shadowsocksRb@users.noreply.github.com>
2019-10-25 18:03:42 +0800

    任务执行优化

--- core/src/main/java/com/github/shadowsocks/work/SSRSubSyncer.kt
+++ core/src/main/java/com/github/shadowsocks/work/SSRSubSyncer.kt
@@ -1,18 +1,11 @@
 package com.github.shadowsocks.work
 
 import android.content.Context
-import android.util.Base64
 import androidx.work.*
 import com.github.shadowsocks.Core
-import com.github.shadowsocks.database.Profile
-import com.github.shadowsocks.database.ProfileManager
-import com.github.shadowsocks.database.SSRSub
 import com.github.shadowsocks.database.SSRSubManager
 import com.github.shadowsocks.utils.printLog
-import com.github.shadowsocks.utils.useCancellable
 import java.io.IOException
-import java.net.HttpURLConnection
-import java.net.URL
 import java.util.concurrent.TimeUnit
 
 class SSRSubSyncer(context: Context, workerParams: WorkerParameters) : CoroutineWorker(context, workerParams) {
@@ -20,8 +13,9 @@ class SSRSubSyncer(context: Context, workerParams: WorkerParameters) : Coroutine
         private const val NAME = "ssrSubAllUpdate"
 
         fun enqueue() = WorkManager.getInstance(Core.deviceStorage).enqueueUniquePeriodicWork(
-                NAME, ExistingPeriodicWorkPolicy.REPLACE,
+                NAME, ExistingPeriodicWorkPolicy.KEEP,
                 PeriodicWorkRequestBuilder<SSRSubSyncer>(1, TimeUnit.DAYS).run {
+                    setBackoffCriteria(BackoffPolicy.EXPONENTIAL, 1, TimeUnit.MINUTES)
                     setConstraints(Constraints.Builder()
                             .setRequiredNetworkType(NetworkType.CONNECTED)
                             .setRequiresCharging(false)
@@ -37,7 +31,10 @@ class SSRSubSyncer(context: Context, workerParams: WorkerParameters) : Coroutine
         SSRSubManager.updateAll()
         Result.success()
     } catch (e: IOException) {
-        e.printStackTrace()
-        if (runAttemptCount > 5) Result.failure() else Result.retry()
+        printLog(e)
+        if (runAttemptCount > 5) {
+            cancel()
+            Result.failure()
+        } else Result.retry()
     }
 }
--- core/src/main/java/com/github/shadowsocks/work/UpdateCheck.kt
+++ core/src/main/java/com/github/shadowsocks/work/UpdateCheck.kt
@@ -1,7 +1,10 @@
 package com.github.shadowsocks.work
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.getSystemService
@@ -10,6 +13,7 @@ import com.github.shadowsocks.Core
 import com.github.shadowsocks.Core.app
 import com.github.shadowsocks.core.BuildConfig
 import com.github.shadowsocks.core.R
+import com.github.shadowsocks.utils.printLog
 import com.github.shadowsocks.utils.useCancellable
 import com.google.gson.JsonStreamParser
 import java.net.HttpURLConnection
@@ -23,7 +27,7 @@ class UpdateCheck(context: Context, workerParams: WorkerParameters) : CoroutineW
         const val url = "https://raw.githubusercontent.com/shadowsocksRb/shadowsocksRb-android/master/update.json"
 
         fun enqueue() = WorkManager.getInstance(Core.deviceStorage).enqueueUniquePeriodicWork(
-                "UpdateCheck", ExistingPeriodicWorkPolicy.REPLACE,
+                "UpdateCheck", ExistingPeriodicWorkPolicy.KEEP,
                 PeriodicWorkRequestBuilder<UpdateCheck>(1, TimeUnit.DAYS).run {
                     setConstraints(Constraints.Builder()
                             .setRequiredNetworkType(NetworkType.UNMETERED)
@@ -39,17 +43,20 @@ class UpdateCheck(context: Context, workerParams: WorkerParameters) : CoroutineW
         val info = JsonStreamParser(json).asSequence().single().asJsonObject
         if (info["version"].asInt > BuildConfig.VERSION_CODE) {
             val nm = app.getSystemService<NotificationManager>()!!
+            val intent= Intent(Intent.ACTION_VIEW).setData(Uri.parse(info["uri"].asString))
             val builder = NotificationCompat.Builder(app as Context, "update")
                     .setColor(ContextCompat.getColor(app, R.color.material_primary_500))
+                    .setContentIntent(PendingIntent.getActivity(app,0,intent,0))
                     .setSmallIcon(R.drawable.ic_service_active)
                     .setCategory(NotificationCompat.CATEGORY_STATUS)
                     .setContentTitle(info["title"].asString)
                     .setContentText(info["text"].asString)
+                    .setAutoCancel(true)
             nm.notify(62, builder.build())
         }
         Result.success()
     } catch (e: Exception) {
-        e.printStackTrace()
+        printLog(e)
         if (runAttemptCount > 5) Result.failure() else Result.retry()
     }
 }
