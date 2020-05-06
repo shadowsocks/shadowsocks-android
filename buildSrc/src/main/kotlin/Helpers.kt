053206d1e786df2d1d5fef8570d889725310c433
Max Lv <max.c.lv@gmail.com>
2020-05-06 15:58:23 +0800

    Bump version

--- buildSrc/src/main/kotlin/Helpers.kt
+++ buildSrc/src/main/kotlin/Helpers.kt
@@ -50,8 +50,8 @@ fun Project.setupCore() {
     setupCommon()
     android.apply {
         defaultConfig {
-            versionCode = 5010050
-            versionName = "5.1.0-nightly"
+            versionCode = 5010150
+            versionName = "5.1.1-nightly"
         }
         compileOptions.isCoreLibraryDesugaringEnabled = true
         ndkVersion = "21.1.6352462"
