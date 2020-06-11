8f445bc13a70899faaeab85021739175039e64ae
Mygod <contact-git@mygod.be>
2020-06-11 08:42:21 +0800

    Update to Android 11 beta 1

--- buildSrc/src/main/kotlin/Helpers.kt
+++ buildSrc/src/main/kotlin/Helpers.kt
@@ -24,10 +24,10 @@ val Project.currentFlavor get() = gradle.startParameter.taskRequests.toString().
 
 fun Project.setupCommon() {
     android.apply {
-        compileSdkVersion("android-R")
+        compileSdkVersion(30)
         defaultConfig {
             minSdkVersion(21)
-            targetSdkVersion("R")
+            targetSdkVersion(30)
             testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         }
         val javaVersion = JavaVersion.VERSION_1_8
