8a70231f699d4159ec243e1905540e161fb398d8
Mygod <contact-git@mygod.be>
2020-07-17 04:31:21 +0800

    Update Android gradle

--- repositories.gradle.kts
+++ repositories.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.extra.apply {
-    set("androidPlugin", "com.android.tools.build:gradle:4.1.0-beta03")
+    set("androidPlugin", "com.android.tools.build:gradle:4.1.0-beta04")
     set("kotlinVersion", "1.3.72")
 }
 
