15c70ae9b82348fd08df8b11b38c8e7c8053173f
Goooler <wangzongler@gmail.com>
2021-02-05 15:05:47 +0800

    Update dependencies

--- repositories.gradle.kts
+++ repositories.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.extra.apply {
     set("androidPlugin", "com.android.tools.build:gradle:4.1.2")
-    set("kotlinVersion", "1.4.21")
+    set("kotlinVersion", "1.4.30")
 }
 
 repositories {
