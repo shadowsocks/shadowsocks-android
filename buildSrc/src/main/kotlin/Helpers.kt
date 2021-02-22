49f3891b90ef1df32b322cc1eb27dce1b4f6feec
Max Lv <max.c.lv@gmail.com>
2021-02-22 18:54:25 +0800

    Bump version

--- buildSrc/src/main/kotlin/Helpers.kt
+++ buildSrc/src/main/kotlin/Helpers.kt
@@ -56,8 +56,8 @@ fun Project.setupCore() {
     setupCommon()
     android.apply {
         defaultConfig {
-            versionCode = 5020150
-            versionName = "5.2.1-nightly"
+            versionCode = 5020250
+            versionName = "5.2.2-nightly"
         }
         compileOptions.isCoreLibraryDesugaringEnabled = true
         lintOptions {
