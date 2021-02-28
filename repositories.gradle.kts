7955472d36f68ccb2950be0f2399feb6807fdd87
Goooler <wangzongler@gmail.com>
2021-02-28 12:01:16 +0800

    Bump kotlin to 1.4.31

--- repositories.gradle.kts
+++ repositories.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.extra.apply {
     set("androidPlugin", "com.android.tools.build:gradle:4.1.2")
-    set("kotlinVersion", "1.4.30")
+    set("kotlinVersion", "1.4.31")
 }
 
 repositories {
