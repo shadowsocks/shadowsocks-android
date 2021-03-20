490e4be8b4b410b272394f05f403b5961463983a
Goooler <wangzongler@gmail.com>
2021-03-20 17:43:18 +0800

    Bump AGP to 4.1.3

--- repositories.gradle.kts
+++ repositories.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.extra.apply {
-    set("androidPlugin", "com.android.tools.build:gradle:4.1.2")
+    set("androidPlugin", "com.android.tools.build:gradle:4.1.3")
     set("kotlinVersion", "1.4.31")
 }
 
