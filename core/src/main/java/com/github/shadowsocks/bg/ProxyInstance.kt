8ce6bd6716ce2a5421b44e7467a5c069b00f46b8
Mygod <contact-git@mygod.be>
2021-04-16 08:20:49 +0800

    Use default udp_timeout=300 instead
    Refine #2625.

--- core/src/main/java/com/github/shadowsocks/bg/ProxyInstance.kt
+++ core/src/main/java/com/github/shadowsocks/bg/ProxyInstance.kt
@@ -83,7 +83,6 @@ class ProxyInstance(val profile: Profile, private val route: String = profile.ro
             }
             config.put("plugin", path).put("plugin_opts", opts.toString())
         }
-        config.put("udp_max_associations", 256)
         config.put("dns", "unix://local_dns_path")
 
         // init local config array
