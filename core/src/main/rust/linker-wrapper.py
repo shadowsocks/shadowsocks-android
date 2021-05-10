6da9cbbcb480c07c5bc86949d4bff5d157ad4b61
SquallATF <squallatf@gmail.com>
2021-05-10 15:59:44 +0800

    Allowed build under msys2 or cygwin

--- core/src/main/rust/linker-wrapper.py
+++ core/src/main/rust/linker-wrapper.py
@@ -6,7 +6,46 @@ import shutil
 import subprocess
 import sys
 
-args = [os.environ['RUST_ANDROID_GRADLE_CC'], os.environ['RUST_ANDROID_GRADLE_CC_LINK_ARG']] + sys.argv[1:]
+rustcc = os.environ['RUST_ANDROID_GRADLE_CC']
+
+if sys.platform == 'msys' or sys.platform == 'cygwin':
+    import ctypes
+
+    cygdll = 'cygwin1.dll' if sys.platform == 'cygwin' else 'msys-2.0.dll'
+    cygwin = ctypes.cdll.LoadLibrary(cygdll)
+
+    def win2posix(path):
+        CCP_WIN_W_TO_POSIX = 3
+        size = cygwin.cygwin_conv_path(CCP_WIN_W_TO_POSIX, path, 0, 0)
+        retval = ctypes.create_string_buffer(size)
+        cygwin.cygwin_conv_path(CCP_WIN_W_TO_POSIX, path, retval, size)
+        return retval.value.decode()
+
+    rustcc = win2posix(rustcc)
+
+args = [rustcc, os.environ['RUST_ANDROID_GRADLE_CC_LINK_ARG']] + sys.argv[1:]
+
+linkargfileName = ''
+if (sys.platform == 'msys' or sys.platform == 'cygwin') and len(''.join(args)) > 8191:
+    import codecs
+    import tempfile
+
+    def posix2win(path):
+        CCP_POSIX_TO_WIN_W = 1
+        size = cygwin.cygwin_conv_path(CCP_POSIX_TO_WIN_W, str(path).encode(), 0, 0)
+        retval = ctypes.create_unicode_buffer(size)
+        cygwin.cygwin_conv_path(CCP_POSIX_TO_WIN_W, str(path).encode(), retval, size)
+        return retval.value
+
+    # response file should be use UTF-16 with BOM
+    linkargfile = tempfile.NamedTemporaryFile(delete=False)
+    linkargfile.write(codecs.BOM_UTF16_LE)
+    linkargfile.write('\n'.join(sys.argv[1:]).encode('utf-16-le'))
+    linkargfile.close()
+    linkargfileName = linkargfile.name
+    linkargfileNameW = posix2win(linkargfileName)
+    args = [rustcc, os.environ['RUST_ANDROID_GRADLE_CC_LINK_ARG'], '@' + linkargfileNameW]
+
 
 # This only appears when the subprocess call fails, but it's helpful then.
 printable_cmd = ' '.join(pipes.quote(arg) for arg in args)
@@ -21,4 +60,6 @@ if code == 0:
             with open(linkargs[0][1:]) as f:
                 sys_argv = f.read().splitlines()
     shutil.copyfile(sys_argv[sys_argv.index('-o') + 1], os.environ['RUST_ANDROID_GRADLE_TARGET'])
+if linkargfileName != '':
+    os.unlink(linkargfileName)
 sys.exit(code)
