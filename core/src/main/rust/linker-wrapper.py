4597823105e4548b759f8bbbc3a328a2bbca3602
SquallATF <squallatf@gmail.com>
2021-05-10 15:58:18 +0800

    Fix windows build failed #2666 #2711

--- core/src/main/rust/linker-wrapper.py
+++ core/src/main/rust/linker-wrapper.py
@@ -14,5 +14,11 @@ print(printable_cmd)
 
 code = subprocess.call(args)
 if code == 0:
-    shutil.copyfile(sys.argv[sys.argv.index('-o') + 1], os.environ['RUST_ANDROID_GRADLE_TARGET'])
+    sys_argv = sys.argv
+    if sys.platform == 'msys' or sys.platform == 'cygwin' or sys.platform == 'win32':
+        linkargs = list(filter(lambda s: s.startswith('@') and s.find('linker-arguments') != -1, sys.argv[1:]))
+        if linkargs != []:
+            with open(linkargs[0][1:]) as f:
+                sys_argv = f.read().splitlines()
+    shutil.copyfile(sys_argv[sys_argv.index('-o') + 1], os.environ['RUST_ANDROID_GRADLE_TARGET'])
 sys.exit(code)
