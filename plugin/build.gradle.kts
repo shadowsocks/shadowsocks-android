bbc908a566ea20d0e9b1557fc0f8e6c273c453d8
Max Lv <max.c.lv@gmail.com>
2021-05-11 09:26:03 +0800

    Update the maven publish plugin (#2734)
    1. Remove the unnecessary custom URL
    2. Replace jcenter with mavenCentral

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -17,8 +17,6 @@ android {
 }
 
 mavenPublish.targets.getByName("uploadArchives") {
-    releaseRepositoryUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-    snapshotRepositoryUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
     repositoryUsername = findProperty("NEXUS_USERNAME").toString()
     repositoryPassword = findProperty("NEXUS_PASSWORD").toString()
 }
