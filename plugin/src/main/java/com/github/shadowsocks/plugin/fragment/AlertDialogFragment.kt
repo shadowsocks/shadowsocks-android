8c2686a2005e28daebb09091268fe7e7efe21fc5
Mygod <contact-git@mygod.be>
2021-06-01 01:47:53 +0800

    Use MaterialAlertDialogBuilder

--- plugin/src/main/java/com/github/shadowsocks/plugin/fragment/AlertDialogFragment.kt
+++ plugin/src/main/java/com/github/shadowsocks/plugin/fragment/AlertDialogFragment.kt
@@ -29,6 +29,7 @@ import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.setFragmentResultListener
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 /**
  * Based on: https://android.googlesource.com/platform/packages/apps/ExactCalculator/+/8c43f06/src/com/android/calculator2/AlertDialogFragment.java
@@ -62,7 +63,7 @@ abstract class AlertDialogFragment<Arg : Parcelable, Ret : Parcelable?> :
     fun key(resultKey: String = javaClass.name) = args().putString(KEY_RESULT, resultKey)
 
     override fun onCreateDialog(savedInstanceState: Bundle?): AlertDialog =
-        AlertDialog.Builder(requireContext()).also { it.prepare(this) }.create()
+        MaterialAlertDialogBuilder(requireContext()).also { it.prepare(this) }.create()
 
     override fun onClick(dialog: DialogInterface?, which: Int) {
         setFragmentResult(resultKey ?: return, Bundle().apply {
