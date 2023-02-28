487c89ebf5ab4ae69023992bdc5b6f68b4a2a65e
dev4u <dev4u@users.noreply.github.com>
2023-02-28 23:20:11 +0800

    Fix password check
    Fix base64 password check.
    Update dependencies.

--- core/src/main/java/com/github/shadowsocks/bg/ProxyInstance.kt
+++ core/src/main/java/com/github/shadowsocks/bg/ProxyInstance.kt
@@ -36,7 +36,6 @@ import org.json.JSONObject
 import java.io.File
 import java.net.URI
 import java.net.URISyntaxException
-import java.util.function.Consumer
 
 /**
  * This class sets up environment for ss-local.
@@ -51,15 +50,17 @@ class ProxyInstance(val profile: Profile, private val route: String = profile.ro
             "cipher ${profile.method} is deprecated."
         }
         // check the key format for aead-2022-cipher
-        profile.password.split(":").forEach(Consumer {
-            require(profile.method !in setOf(
+        if (profile.method !in setOf(
                 "2022-blake3-aes-128-gcm",
                 "2022-blake3-aes-256-gcm",
                 "2022-blake3-chacha20-poly1305",
-            ) || Base64.decode(it, Base64.DEFAULT).size in arrayOf(16, 32)) {
-                "The Base64 Key is invalid."
+            )) {
+            for (pwd in profile.password.split(":")) {
+                require(Base64.decode(pwd, Base64.DEFAULT).size in arrayOf(16, 32)) {
+                    "The Base64 Key is invalid."
+                }
             }
-        })
+        }
     }
 
     private var configFile: File? = null
